b0541731eeeb4b560791b1cf792bdc1c6b4655d9
oumaimaneffati <oumaymaneffati@gmail.com>
2020-03-17 22:32:50 +0100

    first commit

---
+++ admin/config/config.inc.php
@@ -0,0 +1,12 @@
+<?php
+$conf['db_host'] = "localhost";
+$conf['db_user'] = "root";
+$conf['db_passwort'] = "ouma1995";
+$conf['db_name'] = "websoccer";
+$conf['db_prefix'] = "";
+$conf['supported_languages'] = "de,en";
+$conf['homepage'] = "http://websoccer";
+$conf['context_root'] = "";
+$conf['projectname'] = "websoccer";
+$conf['systememail'] = "oumaymaneffati@gmail.com";
+?>
